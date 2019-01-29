ffba985fd740ff918f1b35d0792bb78bd5e463b2
Will Schulz <will.schulz29@gmail.com>
2019-01-29 13:08:48 -0600

    initial pull request

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // Your code goes here
+// const myMouseOver = document.getElementsByClassName('.nav-link');
+
+// myMouseOver.addEventListener('mouseover', function(event) {
+//     console.log(event);
+// });
+
+
+const navBarHover = document.querySelectorAll('a');
+
+navBarHover.forEach(element => {
+    element.addEventListener('mouseover', navBarHover => {
+        navBarHover.stopPropagation();
+        navBarHover.target.style.color = "red";
+     })
+     element.addEventListener('mouseleave', navBarHover => {
+        navBarHover.stopPropagation();
+        navBarHover.target.style.color = "#212529";
+     })
+});
